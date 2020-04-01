692191a9f4d1abdaa8a31be94b80f74bc580acaf
mattc <mcorgan@hotpads.com>
2020-03-31 19:30:02 -0700

    client props in code

--- src/main/java/orgpackage/cloneme/CloneMeBootstrap.java
+++ src/main/java/orgpackage/cloneme/CloneMeBootstrap.java
@@ -1,13 +1,17 @@
 package orgpackage.cloneme;
 
+import java.util.Properties;
+
 import javax.inject.Singleton;
 import javax.servlet.annotation.WebListener;
 
 import org.apache.logging.log4j.web.Log4jServletContextListener;
 
 import io.datarouter.client.mysql.config.DatarouterMysqlPlugin.DatarouterMysqlPluginBuilder;
+import io.datarouter.client.mysql.factory.MysqlClientOptionsBuilder;
 import io.datarouter.secret.config.DatarouterSecretPlugin.DatarouterSecretPluginBuilder;
 import io.datarouter.storage.client.ClientId;
+import io.datarouter.storage.client.ClientOptionsFactory;
 import io.datarouter.storage.servertype.BaseServerTypes;
 import io.datarouter.storage.servertype.ServerType;
 import io.datarouter.storage.servertype.ServerTypeDetector.NoOpServerTypeDetector;
@@ -17,22 +21,50 @@ import io.datarouter.web.config.DatarouterWebListener;
 import io.datarouter.web.config.DatarouterWebWebappBuilder;
 import io.datarouter.web.config.DatarouterWebappConfig;
 import io.datarouter.web.filter.https.InsecureHttpsConfiguration;
+import orgpackage.cloneme.CloneMeBootstrap.CloneMeClientIds.CloneMeClientOptionsFactory;
 
 public class CloneMeBootstrap implements DatarouterBootstrap{
 
 	@Singleton
 	public static class CloneMeServerType extends BaseServerTypes{
+		
 		private static final ServerType CLONE_ME = makeProduction("clone-me");
 
 		public CloneMeServerType(){
 			super(CLONE_ME);
 		}
+		
 	}
 
 	public static class CloneMeClientIds{
+		
 		public static final ClientId MYSQL = new ClientId("mysql", true, false);
-	}
 
+		@Singleton
+		public class CloneMeClientOptionsFactory implements ClientOptionsFactory{
+
+			private static final String MYSQL_SCHEMA = "cloneme";
+
+			@Override
+			public Properties getInternalConfigDirectoryTypeOptions(String internalConfigDirectory){
+				switch(internalConfigDirectory){
+				case "dev-docker":
+					return buildDevDockerOptions();
+				default:
+					return new Properties();
+				}
+			}
+
+			private Properties buildDevDockerOptions(){
+				return new MysqlClientOptionsBuilder(MYSQL)
+						.withUrl("percona.docker:3306/" + MYSQL_SCHEMA)
+						.build();
+			}
+
+		}
+		
+	}
+	
 	public static final DatarouterWebappConfig CONFIG = new DatarouterWebWebappBuilder(
 			CloneMeDatarouterService.CLONE_ME,
 			new CloneMeServerType(),
@@ -43,15 +75,18 @@ public class CloneMeBootstrap implements DatarouterBootstrap{
 			.addWebPlugin(new DatarouterSecretPluginBuilder(CloneMeClientIds.MYSQL).build())
 			.addWebPlugin(new DatarouterTaskTrackerPluginBuilder(CloneMeClientIds.MYSQL).build())
 			.setAuthenticationConfig(CloneMeAuthenticationConfig.class)
+			.setClientOptionsFactory(CloneMeClientOptionsFactory.class)
 			.setHttpsConfiguration(InsecureHttpsConfiguration.class)
 			.setServerTypeDetector(NoOpServerTypeDetector.class)
 			.build();
 
 	@WebListener
 	public static class CloneMeWebListener extends DatarouterWebListener{
+		
 		public CloneMeWebListener(){
 			super(CONFIG);
 		}
+		
 	}
 
 }
